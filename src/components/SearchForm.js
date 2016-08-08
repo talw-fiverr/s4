35b4884b0c624014548aeffbb5786222421996ab
Tal Waserman <tal.w@fiverr.com>
2016-08-08 23:30:42 +0300

    worked on the search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -20,6 +20,55 @@ export default class SearchForm extends Component {
       <div>
         <div className="app-search-form-wrapper"></div>
         <div className="app-search-form-inner">
+          <a href="!#" className="search-btn small-button">חפש</a>
+          <select dir="rtl">
+            <option value="" disabled selected>מין</option>
+            <option>בנים</option>
+            <option>בנות</option>
+            <option>מעורב</option>
+          </select>
+          <select dir="rtl">
+            <option value="" disabled selected>טווח גילאים</option>
+            <option>10-12</option>
+            <option>12-14</option>
+            <option>14-16</option>
+            <option>16-18</option>
+            <option>18-25</option>
+            <option>25-35</option>
+            <option>35-45</option>
+            <option>45-55</option>
+            <option>55-65</option>
+            <option>65-75</option>
+          </select>
+          <select dir="rtl">
+            <option value="" disabled selected>גודל הקבוצה</option>
+            <option>1-5</option>
+            <option>5-10</option>
+            <option>10-20</option>
+            <option>20-40</option>
+            <option>40-80</option>
+            <option>80-120</option>
+          </select>
+          <select dir="rtl">
+            <option value="" disabled selected>סוג הקבוצה</option>
+            <option>מותאם אישית</option>
+            <option>אחרי הריון</option>
+            <option>להזיז קצת את הגוף</option>
+            <option>חוזרים לכושר</option>
+            <option>תחרותי</option>
+          </select>
+          <select dir="rtl">
+            <option value="" disabled selected>עיר</option>
+            <option>פרדס חנה</option>
+            <option>כרכור</option>
+            <option>נתניה</option>
+          </select>
+          <select dir="rtl" className="category">
+            <option value="" disabled selected>קטגוריה</option>
+            <option>ריצה</option>
+            <option>שחיה</option>
+            <option>הליכה</option>
+          </select>
           <input
             type="text"
             placeholder="שם קבוצה"
@@ -27,7 +76,6 @@ export default class SearchForm extends Component {
             value={this.state.term}
             onChange={this.onInputChange}>
           </input>
-          <h1>{this.state.term}</h1>
         </div>
       </div>
     );
